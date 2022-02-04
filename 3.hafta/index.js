abae11f3ff16cd1fd554c3622e8ac44c35f0f57a
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-02-04 13:50:29 +0300

    teamwork 3

---
+++ 3.hafta/index.js
@@ -0,0 +1,34 @@
+// -------soru 4------
+var a = ['dog','lion','hen']; 
+a[100] = 'horse';
+console.log(a);
+
+// -------soru 6------
+let discountPrice = function (price) { 
+    return price * 0.85; 
+}; 
+// let function = discountPrice(price) { 
+//     return price * 0.85; 
+//   }; 
+console.log(discountPrice(50));
+
+
+//-------soru 9-------
+// "use strict";
+// console.log(myFunction());
+
+// function myFunction() {
+//   y = 3.14;   // This will also cause an error because y is not declared
+//   return y
+// }
+
+
+// y = 5;
+// console.log(y);
+
+// ------soru 10----------
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(NaN));
+// console.log(Boolean("false"));
+
